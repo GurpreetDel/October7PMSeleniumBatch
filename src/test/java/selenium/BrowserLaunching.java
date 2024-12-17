880f7796db009daf3f054b2b859bdb5b8006f9af
alok2020 <alokkhare2020@gmail.com>
2024-12-17 23:53:14 +0530

    Initial commit

---
+++ src/test/java/selenium/BrowserLaunching.java
@@ -0,0 +1,19 @@
+package selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserLaunching {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		
+	}
+
+}
